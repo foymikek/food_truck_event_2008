3cd47943f63dfa9d96699945b3816cdf3dae0515
Michael Foy <foymikek@gmail.com>
2020-09-15 13:48:06 -0600

    Add remaning tests for test_food_truck_can_stock_item

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -21,8 +21,15 @@ class FoodTruckTest < Minitest::Test
   def test_food_truck_can_stock_item
     food_truck = FoodTruck.new("Rocky Mountain Pies")
     item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
     assert_equal 30, food_truck.stock(item1, 30)
     expected = {item1 => 30}
     assert_equal expected, food_truck.inventory
+    assert_equal 30, food_truck.check_stock(item1)
+    food_truck.stock(item1, 25)
+    assert_equal 55, food_truck.check_stock(item1)
+    food_truck.stock(item2, 12)
+    expected_1 = {item1 => 55, item2 => 12}
+    assert_equal expected_1, food_truck.inventory
   end
 end
