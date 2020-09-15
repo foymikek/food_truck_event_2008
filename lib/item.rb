570bddd00d7bba9e12f1eb257a6bcb8acc0f95fb
Michael Foy <foymikek@gmail.com>
2020-09-15 13:16:36 -0600

    Add passing test_item_attributes test

---
+++ lib/item.rb
@@ -0,0 +1,11 @@
+class Item
+  attr_reader :name
+  def initialize(data)
+    @name = data[:name]
+    @price = data[:price]
+  end
+
+  def price
+    (@price.gsub("$", '')).to_f
+  end
+end
---
+++ test/food_truck_test.rb
@@ -0,0 +1,8 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/item"
+require "./lib/food_truck"
+
+class ItemTest < Minitest::Test
+
+end
