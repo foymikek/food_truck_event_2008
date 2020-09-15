f0e50cc4b88de72b1dc5c37cabc3df5dc893cf3a
Michael Foy <foymikek@gmail.com>
2020-09-15 13:41:42 -0600

    Add passing test test_food_truck_can_stock_item

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -13,4 +13,12 @@ class FoodTruck
       end
   end
 
+  def stock(item, amount)
+    if @inventory[item].nil?
+      @inventory[item] = amount
+    else
+      @inventory[item] += amount
+    end
+  end
+
 end
--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -22,5 +22,7 @@ class FoodTruckTest < Minitest::Test
     food_truck = FoodTruck.new("Rocky Mountain Pies")
     item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
     assert_equal 30, food_truck.stock(item1, 30)
+    expected = {item1 => 30}
+    assert_equal expected, food_truck.inventory
   end
 end
